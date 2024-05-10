6abb07d8e2965bb335f4235e4f77ba17ca41b3ea
memu18 <78782031+memu18@users.noreply.github.com>
2024-05-10 23:19:50 +0900

    Add files via upload

--- v1/load.js
+++ v1/load.js
@@ -7,3 +7,13 @@ if (getParam('d')) {
 else{
 	window.location.href = '?d=Target-1900-6';
 }
+
+const cookies = document.cookie;
+const array = cookies.split(';');
+
+array.forEach(function(value) {
+	const content = value.split('=');
+	if (content[0]==' removed_items'){
+		r_array = content[1].split(',');
+	}
+  })
