c13821d48be0e3dce1f83bfe1dd763ac61091634
memu18 <78782031+memu18@users.noreply.github.com>
2024-05-28 15:40:03 +0900

    Add files via upload

--- load.js
+++ load.js
@@ -8,15 +8,23 @@ else{
 	window.location.href = '?d=Target-1900-6';
 }
 
-const cookies = document.cookie;
-const array = cookies.split(';');
-
-array.forEach(function(value) {
-	const content = value.split('=');
-	if (content[0]==' r_array'){
-		const r_ = content[1].split(',');
-		if(r_ != '')r_.forEach(r => {
-			r_array.push(parseInt(r))
-		});
+if(getParam('c')){
+	let c = getParam('c');
+	if(c==1){
+		const cookies = document.cookie;
+		const array = cookies.split(';');
+	
+		array.forEach(function(value) {
+			const content = value.split('=');
+			if (content[0]==' r_array'){
+				const r_ = content[1].split(',');
+				if(r_ != '')r_.forEach(r => {
+					r_array.push(parseInt(r))
+				});
+			}
+		})
 	}
-  })
+}
+else{
+	window.location.href = location.search + '&c=0';
+}
